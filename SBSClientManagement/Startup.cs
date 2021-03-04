664a54c8862649f39f846f1a747bc92538406036
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2021-03-04 04:50:08 +0100

    Adding AutoMapper to Map Models to ViewModel

--- SBSClientManagement/Startup.cs
+++ SBSClientManagement/Startup.cs
@@ -13,6 +13,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace SBSClientManagement
 {
@@ -40,6 +41,7 @@ namespace SBSClientManagement
             services.AddScoped<ISqlServerRepo, SqlServerRepo>();
 
             services.AddControllersWithViews();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddRazorPages();
         }
 
