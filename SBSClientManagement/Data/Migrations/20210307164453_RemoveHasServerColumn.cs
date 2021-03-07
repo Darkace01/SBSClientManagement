04488587a96a30efe21be6b9f59fec26da67d165
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-03-07 17:56:01 +0100

    Removed HasServer Column

---
+++ SBSClientManagement/Data/Migrations/20210307164453_RemoveHasServerColumn.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SBSClientManagement.Data.Migrations
+{
+    public partial class RemoveHasServerColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasServer",
+                table: "SQLServers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasServer",
+                table: "SQLServers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
