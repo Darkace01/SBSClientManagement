fb53d02f201d5610f74bd5154c107ecd35f94d9a
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-03-16 14:36:08 +0100

    Copy to Clipboard and Display Alert
    Copy to Clipboard using Clipboard.js and also Alertify.js for alerts

--- SBSClientManagement/wwwroot/js/site.js
+++ SBSClientManagement/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var clipboard = new ClipboardJS('.copy');
 
-// Write your JavaScript code.
+clipboard.on('success', function (e) {
+    alertify.notify('copied', 'success', 5, function () { });
+});
+
+clipboard.on('error', function (e) {
+    alertify.notify('error', 'error', 5, function () { });
+});
